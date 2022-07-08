d76d86f4c7fd06c7757d21f73d86cb110c560506
Seb Jones <jones.seb95@googlemail.com>
2022-07-08 20:33:00 +0100

    Feature test BuildIndexPage action

--- tests/Feature/Actions/BuildIndexPageTest.php
+++ tests/Feature/Actions/BuildIndexPageTest.php
@@ -1,14 +1,70 @@
 <?php
 
+use App\Actions\BuildIndexPage;
+use App\DTOs\Calendar;
+use App\DTOs\CalendarEntry;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
-beforeAll(function () {
-    Storage::fake();
+beforeEach(function () {
+    Storage::fake(config('filesystems.default'));
 });
 
-afterAll(function () {
+it('Writes an index.html file to output directory', function () {
+    createFakeCalendarCollectionAndInvokeAction();
+
+    Storage::assertExists('index.html');
 });
 
-it('returns one item', function () {
-    expect(true)->toBeTrue();
+it('index.html contains hyperlink to .ics file', function () {
+    $calendars = createFakeCalendarCollectionAndInvokeAction();
+
+    expectIndexHtmlContents()->toContain(
+        '<a href="' . $calendars->first()->filename
+    );
 });
+
+function expectIndexHtmlContents()
+{
+    return expect(Storage::get('index.html'));
+}
+
+function createFakeCalendarCollectionAndInvokeAction():Collection
+{
+    $calendars = createFakeCalendarCollection();
+
+    invokeActionWithCalendars($calendars);
+
+    return $calendars;
+}
+
+
+function createFakeCalendarCollection():Collection
+{
+    return collect([
+        new Calendar(
+            'Test Calendar',
+            collect([
+                now()->startOfMonth(),
+                now()->addMonth()->startOfMonth(),
+            ]),
+            collect(
+                new CalendarEntry(
+                    now()->startOfMonth()->setDay(7),
+                    'Test Entry 1',
+                ),
+                new CalendarEntry(
+                    now()->startOfMonth()->setDay(14),
+                    'Test Entry 2',
+                ),
+            ),
+            'http://localhost:8000/test-calendar',
+        )
+    ]);
+}
+
+function invokeActionWithCalendars(Collection $calendars):void
+{
+    resolve(BuildIndexPage::class)($calendars);
+}
