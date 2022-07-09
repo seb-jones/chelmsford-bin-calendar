8769de265ba9e3960a35e848b3097fe6bb1a99aa
Seb Jones <jones.seb95@googlemail.com>
2022-07-09 15:41:12 +0100

    Add p tag to explain page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,6 +8,8 @@
     <body>
         <h1>Chelmsford Collection Calendars</h1>
 
+        <p><code>.ics</code> files that can be imported into calendar software such as Google Calendar. Includes email notifications the night before.</p>
+
         @foreach ($calendarsByMonths as $months => $calendars)
             <h2>{{ $months }}</h2>
             <ul>
